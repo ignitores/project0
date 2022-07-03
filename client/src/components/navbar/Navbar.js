326f2431e1f05dccf7cb7ccd9d41cc7fa06c3d0a
gouravratod12 <gouravrathod898@gmail.com>
2022-07-03 19:16:44 -0400

    minor changes

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,19 +1,43 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-//import logo from './Rad1{}.jpg'
+import logo from './img/re.jpg';
+import { FaShoppingCart,FaUserAlt } from 'react-icons/fa';
+//import { FiSearch} from 'react-icons/fi';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="Navbar">
-      {/* <span className="nav-logo">hello</span> */}
-      {/*<img src={logo} alt="" className="logo" />*/}
+      <div className="nav-logo">
+     <img src="./img/re.jpg" alt="" className="logo"/></div>
+
+      <div className="search-input">
+        <a href="#" target="_blank" hidden> </a>
+        <label>
+        <input type="checkbox"/>
+        <input type="text"  placeholder="search here..."/>
+        </label>
+       {/* <a>
+        <FiSearch/>
+  </a>*/}
+     
+      </div>
+     
       <div className={`nav-items ${isOpen && "open"}`}>
         <a href="/home">Home</a>
         <a href="/about">About</a>
-        <a href="/service">Service</a>
+        <a href="/gallery">Gallery</a>
         <a href="/contact">Contact</a>
-      </div>
+        
+          <a href="#">
+          <FaShoppingCart/>
+          </a>
+          <a href="#">
+          <FaUserAlt/>
+          </a>
+          
+          </div>
+      
       <div
         className={`nav-toggle ${isOpen && "open"}`}
         onClick={() => setIsOpen(!isOpen)}
