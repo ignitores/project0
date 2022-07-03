f859a1f5e9615154be626e80b1ebfd4bb92aaf44
unknown <hardikchouhan1901@gmail.com>
2022-07-04 00:00:28 +0300

    about.js

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import './about.css'
-import About from '../../components/about/about';
-const About = () => {
-    return (
-        <>
-            <h1>This is About Page</h1>
+// import React from 'react'
+// import './about.css'
+// import About from '../../components/about/about';
+// const About = () => {
+//     return (
+//         <>
+//             <h1>This is About Page</h1>
+//             {/* <About/> */}
             
 
-        </>
-    )
-}
+//         </>
+//     )
+// }
 
-export default About
+// export default About
