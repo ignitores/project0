8feac91b911f232c06cd19e4ab1ab6cccb319528
gouravratod12 <gouravrathod898@gmail.com>
2022-08-29 23:37:25 +0530

    updation in about

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,33 +1,41 @@
-import React from 'react'
+
+import React from "react"; 
+import './about.css'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/Footer/Footer';
-
-const About = () => {
-    return (
-        <>
-       
-            <div className="container">
-                <h1>About us</h1>
-                <hr/>
-                
-            <form method="">
-            <div className="heading">
-            <div className="imgs">
-                        {/*<img src="./imgs/ownerpic.jpg" alt="owner" srcset=""/>*/}
-                        </div>
-                        <div className="col-md-6"></div>
-                        <div className="profile-head">
-                           
-                        </div>
-
-                           </div>
-                           </form>
-                        </div>    
-                        <Navbar/>
-                        <Footer/>         
-        </>
-       
-    )
+import { Container, Row, Col } from "react-bootstrap";
+import { contactConfig } from "./content_option";
+export default function About() {
+  
+  return (
+    <>
+    <Container>
    
+      <Row className="mb-5 mt-3">
+        <Col lg="8">
+          <h1 className="display-4 mb-4">About Us</h1>
+          <hr className="t_border my-4 ml-0 text-left" />
+    
+          <img src="https://image.shutterstock.com/image-photo/cute-hispanic-female-business-owner-260nw-1396533020.jpg"/>
+     </Col>
+      </Row>
+      <br/>
+      <Row className="sec_sp">
+        <Col lg="5" className="mb-5">
+          <h4 className="color_sec py-4">Who are we?</h4>
+           
+          <p>{contactConfig.description}</p>
+        </Col>
+        <Col lg="7" className="d-flex align-items-center">
+          
+       
+        </Col>
+      </Row>
+      
+    </Container>
+    <Navbar/>
+    <Footer/>
+    </>
+   );
 }
-export default About
+
---
+++ client/src/pages/About/content_option.js
@@ -0,0 +1,82 @@
+const logotext = 'Radhika';
+const meta = {
+  title: 'Radhikabakery',
+  description:
+    'A bakery artist',
+};
+
+const introdata = {
+  title: 'Radhikabsakery',
+  animated: {
+    first: 'I love baking',
+    second: 'I make declicous cakes',
+    third: 'create innvoative designs',
+  },
+  description:
+    ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendumLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendumLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum',
+  
+};
+
+const dataabout = {
+  title: 'abit about my self',
+  aboutme:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.',
+};
+const worktimeline = [
+  {
+    jobtitle: 'Designer of week',
+    where: 'YAdfi',
+    date: '2020',
+  },
+  {
+    jobtitle: 'Designer of week',
+    where: 'Jamalya',
+    date: '2019',
+  },
+  {
+    jobtitle: 'Designer of week',
+    where: 'ALquds',
+    date: '2019',
+  },
+];
+
+
+
+const services = [
+  {
+    title: 'ocaasion based',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+  },
+  {
+    title: 'Mobile Apps',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+  },
+  {
+    title: 'Wordpress Design',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: 'abc@gmail.com',
+  YOUR_FONE: '+91123456789',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ',
+  YOUR_SERVICE_ID: 'service_id',
+  YOUR_TEMPLATE_ID: 'template_id',
+  YOUR_USER_ID: 'user_id',
+};
+
+ 
+export {
+  meta,
+  dataabout,
+  worktimeline,
+  services,
+  introdata,
+  contactConfig, 
+  logotext,
+};
